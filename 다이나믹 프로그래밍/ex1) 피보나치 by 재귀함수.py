83c83c324b667a989f2727b60fc72d7e0fde31fb
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-01-06 20:08:00 +0900

    1/6 DP 예제 추가

---
+++ 다이나믹 프로그래밍/ex1) 피보나치 by 재귀함수.py
@@ -0,0 +1,8 @@
+#ex1) 피보나치 by 재귀함수, 210p
+
+def fibo(x) :
+  if x == 1 or x == 2 :
+    return 1
+  return fibo(x-1) + fibo(x-2)
+
+print(fibo(4))
---
+++ 다이나믹 프로그래밍/ex3) 피보나치 호출함수 확인.py
@@ -0,0 +1,16 @@
+#ex3) 피보나치 호출함수 확인, 214p
+
+d = [0]*100
+def fibo(x) :
+  print('f(' + str(x) + ')', end=' ')
+
+  if x == 1 or x == 2 :
+    return 1
+
+  if d[x] != 0 :
+    return d[x]
+
+  d[x] = fibo(x-1) + fibo(x-2)
+  return d[x]
+
+fibo(6)
